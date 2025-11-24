1e1b02ad0ce6072b939727c559d4f84cc6fa0549
Gaozhancheng <Heisenbugx@gmail.com>
2025-11-24 17:08:05 +0800

    Sport注册网页

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/",methods = ["GET","POST"])
+@app.route("/")
 def index():
-    if request.method == "POST":
-        return render_template("greet.html",name=request.form.get("name"))
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
+
+@app.route("/register",methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport"):
+        return render_template("failure.html")
+    return render_template("success.html")
